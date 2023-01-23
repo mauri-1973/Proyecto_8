c306c777ba691b370c38ba446dcb9517b5c1200d
mauri-1973 <mauri-1973@outlook.cl>
2023-01-23 18:29:17 -0300

    Integración y Configuración DataTables

--- app/Models/Vehiculos.php
+++ app/Models/Vehiculos.php
@@ -9,37 +9,22 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class User extends Authenticatable
+class Vehiculos extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
+
+    protected $table = 'vehiculos';
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'users_id_veh',
+        'marca',
+        'patente',
+        'annio',
+        'precio',
     ];
 }
