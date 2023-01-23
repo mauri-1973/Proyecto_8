64ac98bc1b1478fcd46e9f63418cbe498cf575d5
mauri-1973 <mauri-1973@outlook.cl>
2023-01-23 17:07:42 -0300

    Integración multi-idioma(Español-Ingles)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+               <!-- <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
-                </a>
+                </a>-->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -33,6 +33,18 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                        <!--Comprobamos si el status esta a true y existe más de un lenguaje-->
+                        @if (config('locale.status') && count(config('locale.languages')) > 1)
+                            <div class="top-right links">
+                                @foreach (array_keys(config('locale.languages')) as $lang)
+                                    @if ($lang != App::getLocale())
+                                        <a href="{!! route('lang.swap', $lang) !!}">
+                                        {!! trans('messages.lang') !!}<small>&nbsp;{!! trans('messages.lang1') !!}</small>
+                                        </a>
+                                    @endif
+                                @endforeach
+                            </div>
+                        @endif
 
                     </ul>
 
